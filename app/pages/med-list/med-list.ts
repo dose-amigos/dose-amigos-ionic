855349a7fd8317118275ea60ba6e754ad69bde04
jleopold28 <jleopold28@gmail.com>
2016-07-10 16:03:58 -0400

    Updating placeholders

--- app/pages/med-list/med-list.ts
+++ app/pages/med-list/med-list.ts
@@ -1,22 +1,25 @@
-import {Component} from "@angular/core";
+import {Component, OnInit, Type} from "@angular/core";
 import {DoseAmigosToolbar} from "../../dose-amigos-toolbar/dose-amigos-toolbar.component";
-import {NavController} from "ionic-angular";
 import {DoseMedication} from "../../dose-medication/dose-medication";
+// import {MedListCardComponent} from "../../med-list-card/med-list-card.component";
+// import {DoseMedicationService} from "./dose-medication-service/dose-medication.service";
 
 @Component(
     {
         templateUrl: "build/pages/med-list/med-list.html",
-        directives: [DoseAmigosToolbar]
+        directives: [
+            DoseAmigosToolbar
+        ]
     }
 )
-
 export class MedListPage {
 
     public title: string = "My Medications";
-    // public medList: Array<DoseMedication> = [];
+    public doseMedicationList: Array<DoseMedication> = [];
+    public medListClickPage: Type;
 
     constructor(
-        // private medListService: MedListService
+        // private doseMedicationService: DoseMedicationService
     ) {
 
     }
