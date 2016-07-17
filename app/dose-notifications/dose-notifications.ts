58add897070347937632ed8950448b923bd07488
Geoffrey Graddy <geoffrey.graddy@gtri.gatech.edu>
2016-07-17 09:46:09 -0400

    Added dose-notifications to project

--- app/dose-notifications/dose-notifications.ts
+++ app/dose-notifications/dose-notifications.ts
@@ -1,6 +1,19 @@
+import {Component} from "@angular/core";
+import {NavController, Alert} from 'ionic-angular';
 import {LocalNotifications} from 'ionic-native';
 
 export class DoseNotifications {
+
+    constructor(private navController: NavController) {
+        LocalNotifications.on("click", (notification, state) => {
+            let alert = Alert.create({
+                title: "Dose Amigos Medication Notification",
+                subTitle: "You currently have pending medications.",
+                buttons: ["OK"]
+            });
+            this.navController.present(alert);
+        });
+    }
 
     /**
      *  Method for scheduling a daily local notification.
