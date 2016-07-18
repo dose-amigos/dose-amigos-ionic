29fb46c17660f0f6b052f917b0f6498a0464f1a9
jmckoon3 <Jay.McKoon@gtri.gatech.edu>
2016-07-18 12:06:47 -0400

    Issue #49: DoseAmigosUserService AuthHttp Integration
    Updates DoseAmigosUserService to use AuthHttp.
    
    Removes AuthUser and AuthUserService.
    Adds DoseAmigosUserService.getCurrent().
    Updates FeedPage to get current user for status card.
    
    Closes #49.

--- app/pages/feed/feed.ts
+++ app/pages/feed/feed.ts
@@ -3,11 +3,11 @@ import {FeedEvent} from "../../feed-event/feed-event";
 import {FeedEventCardComponent} from "../../feed-event-card/feed-event-card.component";
 import {FeedEventService} from "../../feed-event-service/feed-event-service";
 import {DoseAmigosToolbar} from "../../dose-amigos-toolbar/dose-amigos-toolbar.component";
-import {AuthUserService} from "../../auth-user-service/auth-user.service";
-import {AuthUser} from "../../auth-user/auth-user";
 import {UserStatusCardComponent} from "../../user-status-card/user-status-card.component";
 import {DosePage} from "../dose/dose";
 import {LogonPanelComponent} from "../../logon-panel-component/logon-panel.component";
+import {DoseAmigosUser} from "../../dose-amigos-user/dose-amigos-user";
+import {DoseAmigosUserService} from "../../dose-amigos-user-service/dose-amigos-user.service";
 
 /**
  * Component that renders the Amigos Feed Page of dose events.
@@ -27,21 +27,25 @@ export class FeedPage implements OnInit {
 
     public feedEvents: Array<FeedEvent> = [];
     public title: string = "News Feed";
-    public authUser: AuthUser;
+    public currentUser: DoseAmigosUser;
     public userStatusClickPage: Type;
 
     constructor(
         private feedEventService: FeedEventService,
-        private authUserService: AuthUserService
+        private doseAmigosUserService: DoseAmigosUserService
     ) {
 
     }
 
     public ngOnInit(): any {
 
-        this.authUser = this.authUserService.get();
-
         this.userStatusClickPage = DosePage;
+
+        this.doseAmigosUserService.getCurrent().then(
+            currentUser => {
+                this.currentUser = currentUser;
+            }
+        );
 
         return this.feedEventService.list().then(
             feedEvents => {
